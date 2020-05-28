9169d50c6c35eaf46b63f49d8cbc37f32aeac7ea
azipporah <zipporahatusasiire@gmail.com>
2020-05-28 02:07:37 +0200

    Adding register and login

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//to keep important info from public
+require('dotenv/config');
+// dotenv.config();
+
+//Import routes
+const authRoute = require('./routes/auth');
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/', authRoute);//middleware for the router
+
+//to connect to the server
+app.listen(5000, function(){
+    console.log('server started at port 5000');    
+});
+
+//connect to database
+mongoose.connect(process.env.db_connection, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+    console.log('Connected to db')
+);
